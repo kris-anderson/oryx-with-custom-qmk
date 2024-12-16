9921b7bf19df61f71063d7e3f519bd47162288f5
Kris Anderson <1562427+kris-anderson@users.noreply.github.com>
2024-12-15 21:29:16 -0800

    replace ctrl with gui for achordion eager mod

--- EMbxA/features/achordion.c
+++ EMbxA/features/achordion.c
@@ -345,7 +345,8 @@ __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
 
 // By default, Shift and Ctrl mods are eager, and Alt and GUI are not.
 __attribute__((weak)) bool achordion_eager_mod(uint8_t mod) {
-  return (mod & (MOD_LALT | MOD_LGUI)) == 0;
+  // return (mod & (MOD_LALT | MOD_LGUI)) == 0; // Original
+  return (mod & (MOD_LALT | MOD_LCTRL)) == 0; // Replace CTRL with GUI
 }
 
 #ifdef ACHORDION_STREAK
